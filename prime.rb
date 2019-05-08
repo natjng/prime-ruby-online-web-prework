1d7cecb0cb50babc52da93fb5a9f587c43fd3754
Natalie J <natsetc@gmail.com>
2019-05-08 01:57:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
   if n <= 1
     false
   end
-  Math.sqrt(n).to_i.downto(2).each {|i| if n % i == 0 false}
+  Math.sqrt(n).to_i.downto(2).each {|i| if n % i == 0; false}
   true
 end
 end
