a043bbc99992f7a0122a6d5ddf0554c95e2b3486
Natalie J <natsetc@gmail.com>
2019-05-08 02:04:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(n)
   return false if n <= 1
-  2..Math.sqrt(n).to_i.downto(2).each {|i| return false if n % i == 0}
+  (2..Math.sqrt(n)).to_i.downto.each {|i| return false if n % i == 0}
   true
 end
